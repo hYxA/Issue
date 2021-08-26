39d926efe9a99223943ad27ff226a068d73e8d11
Игорь <zybu@live.ru>
2021-08-26 20:26:44 +0300

    added Issue.java

---
+++ src/main/java/domain/Issue.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Issue {
+    private boolean isClosed;
+    private int number;
+    private String author;
+    private String label;
+    private String assignee;
+}
