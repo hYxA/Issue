136e18cb407072f7d4f89cb9e6ccb91ad3294692
Игорь <zybu@live.ru>
2021-08-27 16:44:09 +0300

    added IssueRepository.java

---
+++ src/main/java/ru/netology/repository/IssueRepository.java
@@ -0,0 +1,11 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Issue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class IssueRepository {
+    private Collection<Issue> items = new ArrayList<>();
+
+}
